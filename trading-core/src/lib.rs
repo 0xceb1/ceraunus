2742d9e3f93d55ad88c443f0bfaeb3785d7a4e87
Darren Weng <wjhdarren@gmail.com>
2025-12-14 19:24:46 +0800

    add a basic QuoteStrategy

--- trading-core/src/lib.rs
+++ trading-core/src/lib.rs
@@ -2,6 +2,7 @@ pub mod models;
 pub mod engine;
 pub mod error;
 pub mod exchange;
+pub mod strategy;
 
 pub use models::OrderBook;
 pub use error::{ApiError, ConnectivityError, Error, Result, TradingCoreError};
---
+++ trading-core/src/strategy.rs
@@ -0,0 +1,27 @@
+use crate::engine::State;
+use crate::models::Order;
+use data::order::*;
+use rust_decimal::dec;
+
+pub trait Strategy {
+    fn generate_quotes(symbol: Symbol, state: &State) -> Vec<Order>;
+}
+
+pub struct QuoteStrategy;
+
+impl Strategy for QuoteStrategy {
+    fn generate_quotes(symbol: Symbol, state: &State) -> Vec<Order> {
+        if let  Some((bid, ask)) = state.get_bbo_level(&symbol) {
+            let spread = ask.price - bid.price;
+            let mid_price = (ask.price + bid.price ) / dec!(2);
+            let ask_opx = mid_price + spread / dec!(2);
+            let bid_opx = mid_price - spread / dec!(2);
+            vec![
+                Order::new(symbol, Side::Buy, OrderKind::Limit, bid_opx, dec!(0.1), TimeInForce::GoodUntilCancel, None),
+                Order::new(symbol, Side::Sell, OrderKind::Limit, ask_opx, dec!(0.1), TimeInForce::GoodUntilCancel, None),
+            ]
+        } else {
+            Vec::new()
+        }
+    }
+}
